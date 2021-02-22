65c449cfa05c68c375ce026d9edd4c828949a4d9
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-02-21 18:45:50 -0600

    Cleaned React files and created config file for firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyCEPjb4vMNeGXvEekKaud-nFyKfoKhtS-k',
+  authDomain: 'instagram-clone-bg.firebaseapp.com',
+  projectId: 'instagram-clone-bg',
+  storageBucket: 'instagram-clone-bg.appspot.com',
+  messagingSenderId: '182120854412',
+  appId: '1:182120854412:web:f4e9df0f6d5361cfcb9ca9',
+};
