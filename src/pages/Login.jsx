7826e812ac57ebc4cd98b80edb1789b74dced79c
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-02-22 19:06:13 -0600

    Finished styling login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,19 +6,38 @@ import * as ROUTES from '../constants/routes';
 const Login = () => {
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="login-image flex w-3/5">
+      <div className="flex w-3/5">
         <img src={loginIphoneImage} alt="iPhone with Instagram app" />
       </div>
-      <div className="login-section flex flex-col w-2/5">
-        <div className="login-info">
-          <img src={loginLogo} alt="Instagram" />
-          <input type="email" name="login-email" id="login-email" placeholder="Email adress" />
-          <input type="password" name="login-password" id="login-password" placeholder="Password" />
-          <button>Log In</button>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src={loginLogo} alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          <form method="POST">
+            <input
+              aria-label="Enter your email adress"
+              className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+              type="email"
+              placeholder="Email adress"
+            />
+            <input
+              aria-label="Enter your password"
+              className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+              type="password"
+              placeholder="Password"
+            />
+            <button type="submit" className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}>
+              Log In
+            </button>
+          </form>
         </div>
-        <div className="signInLink">
-          <p>
-            Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign up</Link>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
+          <p className="text-sm">
+            Don't have an account?{' '}
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-700">
+              Sign up
+            </Link>
           </p>
         </div>
       </div>
