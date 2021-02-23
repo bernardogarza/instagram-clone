bd87c8f86ee1721b5f1019b87f9e98b52b35e2ac
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-02-23 13:44:49 -0600

    created handleLogin function and added useHistory to redirect to dashboarD

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,31 @@
 import { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import loginIphoneImage from '../assets/images/iphone-with-profile.jpg';
 import loginLogo from '../assets/images/logo.png';
 import * as ROUTES from '../constants/routes';
 import FirebaseContext from '../context/firebase';
 
 const Login = () => {
+  const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
+
   const [emailAdress, setEmailAdress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAdress === '';
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAdress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAdress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Login | Instagram Clone';
@@ -27,7 +41,12 @@ const Login = () => {
           <h1 className="flex justify-center w-full">
             <img src={loginLogo} alt="Instagram" className="mt-2 w-6/12 mb-4" />
           </h1>
-          <form method="POST">
+          {error && (
+            <p className="mb-4 text-xs text-red-500">
+              <b>{error}</b>
+            </p>
+          )}
+          <form onSubmit={handleLogin} method="POST">
             <input
               aria-label="Enter your email adress"
               className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
