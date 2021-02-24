1cd4fe50851afae9c7381ec6356fd0765324c9e5
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-02-24 11:46:22 -0600

    Created a mock for the dash board and component files needed

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,9 @@
+const Timeline = () => {
+  return (
+    <div>
+      <p>Timeline</p>
+    </div>
+  );
+};
+
+export default Timeline;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar/index';
+import Timeline from '../components/Timeline';
+
 const Dashboard = () => {
+  useEffect(() => {
+    document.title = 'Instagram';
+  }, []);
   return (
-    <div>
-      <p>Hello from Dashboard</p>
+    <div className="bg-gray-100">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Timeline />
+        <Sidebar />
+      </div>
     </div>
   );
 };
