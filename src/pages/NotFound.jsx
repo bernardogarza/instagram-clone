a1735434fc670cc12f648aaaf524ca58f20cd029
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-02-24 11:54:14 -0600

    Created not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+
 const NotFound = () => {
+  useEffect(() => {
+    document.title = '404 - Not Found';
+  }, []);
+
   return (
-    <div>
-      <p>Hello from Not Found</p>
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found</p>
+      </div>
     </div>
   );
 };
